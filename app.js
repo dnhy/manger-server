b104890d79ce0b57633fb1965c411df56b40088b
1240791903@qq.com <1240791903@qq.com>
2025-02-04 14:20:15 +0800

    feat: 云服务器上线

--- app.js
+++ app.js
@@ -9,13 +9,33 @@ const logger = require("koa-logger");
 const users = require("./routes/users");
 const log4js = require("./utils/log4j");
 const koaJwt = require("koa-jwt");
+const cors = require("koa2-cors");
 const Util = require("./utils/util");
-const { SECRET } = require("./config/options");
+const { SECRET, whiteList } = require("./config/options");
 
 // error handler
 onerror(app);
 
 require("./config/db");
+
+app.use(
+  cors({
+    origin: function (ctx) {
+      let url = ctx.header.referer.substr(0, ctx.header.referer.length - 1);
+      const domin = url?.split("/")[2]?.split(":")[0];
+
+      if (whiteList.includes(domin)) {
+        return url; //注意，这里域名末尾不能带/，否则不成功，所以在之前我把/通过substr干掉了
+      }
+      return "http://localhost:3000"; //默认允许本地请求3000端口可跨域
+    },
+    maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+    credentials: true, //是否允许发送Cookie
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], //设置所允许的HTTP请求方法
+    allowHeaders: ["Content-Type", "Authorization", "Accept"], //设置服务器支持的所有头信息字段
+    exposeHeaders: ["WWW-Authenticate", "Server-Authorization"], //设置获取其他自定义字段
+  })
+);
 
 // middlewares
 app.use(
--- config/options.js
+++ config/options.js
@@ -1,3 +1,4 @@
 module.exports = {
   SECRET: "dnhy",
+  whiteList: ["123.207.204.169", "localhost", "127.0.0.1"], //可跨域白名单
 };
